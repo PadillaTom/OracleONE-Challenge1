346e1efae0e2bde0c35ee8ceb188a8adabb1470d
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-26 15:03:13 +0200

    Copiado OK

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const imgContainer = document.querySelectorAll(".img-container");
 const resultDisplay = document.querySelectorAll(".result-display");
 const resultDisplayP = document.querySelectorAll(".result-display > p");
 const resultDisplayBtn = document.querySelectorAll(".result-display > button");
+const copyBtn = document.querySelectorAll(".copyBtn");
 const restartBtn = document.querySelectorAll(".restartBtn");
 
 // Reemplazos:
@@ -41,38 +42,75 @@ const remakeVowels = (myString) => {
 
 // === Main Functions ===
 
-const encriptado = () => {
+const encriptado = (e) => {
+	e.preventDefault();
 	var myInput = userInput.value;
+
+	// Validations:
+	if (myInput.length <= 0) {
+		alert("Debes ingresar al menos una palabra");
+		return;
+	}
+
+	// Encript:
 	let response = replaceVowels(myInput);
 
+	// Display Handle:
 	imgContainer.forEach((i) => {
 		i.style.display = "none";
 	});
 	resultDisplay.forEach((i) => {
 		i.style.display = "block";
-		i.firstElementChild.innerHTML = response;
+		i.firstElementChild.value = response;
 	});
 };
 
-const desencriptado = () => {
+const desencriptado = (e) => {
+	e.preventDefault();
 	let myInput = userInput.value;
+
+	// Validations:
+	if (myInput.length <= 0) {
+		alert("Debes ingresar al menos una palabra");
+		return;
+	}
+
+	// Decript:
 	let response = remakeVowels(myInput);
 
+	// Display Handle:
 	imgContainer.forEach((i) => {
 		i.style.display = "none";
 	});
 	resultDisplay.forEach((i) => {
 		i.style.display = "block";
-		i.firstElementChild.innerHTML = response;
+		i.firstElementChild.value = response;
 	});
 };
 
 const clearBoard = () => {
 	window.location.reload();
 };
+
+const copyResult = (e) => {
+	e.preventDefault();
+	let resultCopy;
+	resultDisplay.forEach((i) => {
+		resultCopy = i.firstElementChild;
+	});
+	resultCopy.select();
+	resultCopy.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText(resultCopy.value).then(() => {
+		alert("'" + resultCopy.value + "' copiado con éxito.");
+	});
+};
+
 // Events:
 encriptBtn.addEventListener("click", encriptado);
 decriptBtn.addEventListener("click", desencriptado);
 restartBtn.forEach((i) => {
 	i.addEventListener("click", clearBoard);
+});
+copyBtn.forEach((i) => {
+	i.addEventListener("click", copyResult);
 });
